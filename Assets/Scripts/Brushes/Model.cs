901f8970de3d1f61afa752b51caf9ceea22fe751
Eve Li <39169776+evelii@users.noreply.github.com>
2022-07-15 16:42:54 -0400

    replace menu with 3d models

--- Assets/Scripts/Brushes/Model.cs
+++ Assets/Scripts/Brushes/Model.cs
@@ -7,15 +7,20 @@ public class Model : MonoBehaviour
     public GameObject rightHand;
     public MyOutline outline;
 
+    public CanvasHandler canvas;
+    public MenuPanelParent panel;
+    public string myTag;
+
     // Start is called before the first frame update
     void Start()
     {
         outline = gameObject.AddComponent<MyOutline>();
-
         outline.OutlineMode = MyOutline.Mode.OutlineAll;
         outline.OutlineColor = Color.yellow;
         outline.OutlineWidth = 15f;
         outline.enabled = false;
+
+        myTag = gameObject.name;
     }
 
     // Update is called once per frame
@@ -27,17 +32,43 @@ public class Model : MonoBehaviour
         
         if (dis <= 0.1f)
         {
-            Debug.LogWarning(dis);
+            //Debug.LogWarning(dis);
             outline.enabled = true;
+            if (myTag == "SketchButton") panel.sketchButton = true;
+            else if (myTag == "PathButton") panel.pathButton = true;
+            else if (myTag == "MotionButton") panel.motionButton = true;
+            else if (myTag == "SoundButton") panel.soundButton = true;
             if (DrawTubes.buttonOneIsDown)
             {
-                
+                if (myTag == "SketchButton")
+                {
+                    canvas.SketchTaskOnClick();
+                }
+                else if (myTag == "PathButton")
+                {
+                    canvas.PathTaskOnClick();
+                }
+                else if (myTag == "MotionButton")
+                {
+                    canvas.MotionTaskOnClick();
+                }
+                else if (myTag == "SoundButton")
+                {
+                    canvas.SoundTaskOnClick();
+                }
             }
         }
-       
+        else if (canvas.curBrush == myTag)
+        {
+            outline.enabled = true;
+        }
         else
         {
             outline.enabled = false;
+            if (myTag == "SketchButton") panel.sketchButton = false;
+            else if (myTag == "PathButton") panel.pathButton = false;
+            else if (myTag == "MotionButton") panel.motionButton = false;
+            else if (myTag == "SoundButton") panel.soundButton = false;
         }
     }
 }
